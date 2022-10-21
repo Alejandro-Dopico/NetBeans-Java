1eb5bdeccd654e32a0d6c3dd0e63b4c85b178dd7
AlejandroDopico <agarcia2@institutmvm.cat>
2022-10-21 12:24:43 +0200

    java

---
+++ JavaApplication1/src/cat/institutmvm/Exercici_N9.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cat.institutmvm;
+
+import java.util.Scanner;
+
+//Pre: Introdueix un número y especifica si es entre 5 y 50.
+public class Exercici_N9 {
+    private static final String MSG_1 ="Introdueix un número: ";
+            
+    public static void main(String[] args) {
+        int num1;        
+        Scanner sc = new Scanner(System.in);
+        System.out.println(MSG_1);
+        num1 = sc.nextInt();            
+        if(num1 >= 5 && num1 <= 50){
+            System.out.println("El número compleix el rang"); //Printamos directamente en la propia línea
+            }
+        else {
+            System.out.println("El número no compleix el rang");
+          }               
+        }
+}            
+//Post: Ens donara el resultat dintre del rang
+
+
