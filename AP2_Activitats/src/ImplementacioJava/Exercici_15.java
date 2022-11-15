1b9a53e3217836ea4770747dbd2a54c892168b1c
Alejandro-Dopico <114652793+Alejandro-Dopico@users.noreply.github.com>
2022-11-15 21:19:08 +0100

    update

---
+++ AP2_Activitats/src/ImplementacioJava/Exercici_15.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ImplementacioJava;
+
+/*  
+    Alejandro
+    Garcia Dopico
+    INS Manuel Vázquez Montalbán
+    27/10/22
+    Disseny d'Aplicació Web (DAW)
+    Programació  
+ */
+
+//Pre: 
+public class Exercici_15 {
+    
+}
