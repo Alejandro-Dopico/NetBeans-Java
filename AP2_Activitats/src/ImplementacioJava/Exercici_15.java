16882a8a464073fe27b00ef47fdfbddd39f0f1b2
Alejandro-Dopico <114652793+Alejandro-Dopico@users.noreply.github.com>
2022-11-15 23:00:46 +0100

    update

--- AP2_Activitats/src/ImplementacioJava/Exercici_15.java
+++ AP2_Activitats/src/ImplementacioJava/Exercici_15.java
@@ -4,6 +4,8 @@
  */
 package ImplementacioJava;
 
+import java.util.Scanner;
+
 /*  
     Alejandro
     Garcia Dopico
@@ -13,7 +15,56 @@ package ImplementacioJava;
     Programació  
  */
 
-//Pre: 
+//Pre: Inroduim tres valors i executem.
 public class Exercici_15 {
+    private static final String MSG_1 = "Indrodueix tres valors: ";
+    private static final String MSG_2 = "Error, Torna a introduïr un nombre: ";
+    private static final String MSG_3 = "Aquesta es la suma dels tres valors: ";
+    private static final String MSG_4 = "Aquesta es el resultat dels tres valors: ";
     
+    public static void main(String[] args){
+        int num1 = 0, num2 = 0, num3 = 0, resultat = 0;
+        boolean valorCorrecte;
+        Scanner sc = new Scanner(System.in);
+        System.out.println(MSG_1);  
+        do{
+            valorCorrecte = sc.hasNextInt();
+            if(valorCorrecte){
+                num1 = sc.nextInt();
+            }
+            else{
+                sc.next();
+                System.out.println(MSG_2);
+            }
+        }while(!valorCorrecte); 
+        do{
+            valorCorrecte = sc.hasNextInt();
+            if(valorCorrecte){
+                num2 = sc.nextInt();
+            }
+            else{
+                sc.next();
+                System.out.println(MSG_2);
+            }
+        }while(!valorCorrecte); 
+        do{
+            valorCorrecte = sc.hasNextInt();
+            if(valorCorrecte){
+                num3 = sc.nextInt();
+            }
+            else{
+                sc.next();
+                System.out.println(MSG_2);
+            }
+        }while(!valorCorrecte); 
+        if(num1 == num2 && num1 == num3){
+            resultat = num1 + num2 + num3;
+            resultat = resultat * 3;
+            System.out.println(MSG_4 + resultat);
+        }
+        else{
+            resultat = num1 + num2 + num3;
+            System.out.println(MSG_3 + resultat); 
+        }
+    }
 }
