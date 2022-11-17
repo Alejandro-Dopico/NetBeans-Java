93501b3b996dd095ce59c55e41b378897b8bcf8f
AlejandroDopico <agarcia2@institutmvm.cat>
2022-11-17 14:29:19 +0100

    update

---
+++ AP2_Activitats/src/ImplementacioJava/Exercici_19.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ImplementacioJava;
+
+import java.util.Scanner;
+
+/*  
+    Alejandro
+    Garcia Dopico
+    INS Manuel Vázquez Montalbán
+    27/10/22
+    Disseny d'Aplicació Web (DAW)
+    Programació  
+ */
+
+//Pre: Introdueix dos valors i indica quina acció vols fer'hi.
+public class Exercici_19 {
+    private static final String MSG_1 = "Introdueix els valors: ";
+    private static final String MSG_2 = "Error, aquet valor no entra en els parametres: ";
+    private static final String MSG_3 = "Pasem a la seguent fila: ";
+    private static final String MSG_4 = "Printem la matriu tansposta";
+    
+    public static void main(String[] args) {
+        int[][] arrayTwo = new int[];
+        int i;
+        boolean valorCorrecte;
+        Scanner sc = new Scanner(System.in);
+        System.out.println(MSG_1);
+        
+    }
+}
--- UF1_Activitats/src/cat/institutmvm/Exercici30.java
+++ UF1_Activitats/src/cat/institutmvm/Exercici30.java
@@ -15,14 +15,14 @@ import java.util.Scanner;
     Programació  
  */
 
-//Pre: guardem valors en un array.
+//Pre: Guardem valors en un array.
 public class Exercici30 {
 
     private static final String MSG_1 = "Introdueix els valors: " + "\nPrimera fila: ";
     private static final String MSG_2 = "Error, aquet valor no entra en els parametres: ";
     private static final String MSG_3 = "Pasem a la seguent fila: ";
     private static final String MSG_4 = "Printem la matriu tansposta";
-    
+
     public static void main(String[] args) {
         int[][] arrayTwo = new int[7][3];
         int i, j;
@@ -34,28 +34,29 @@ public class Exercici30 {
                 do {
                     valorCorrecte = sc.hasNextInt();
                     if (valorCorrecte) {
-                        do{
+                        do {
                             arrayTwo[i][j] = sc.nextInt();
-                            if(arrayTwo[i][j] < 1 || arrayTwo[i][j] > 9){
+                            if (arrayTwo[i][j] < 1 || arrayTwo[i][j] > 9) {
                                 System.out.println(MSG_2);
                             }
-                        }while (arrayTwo[i][j] <1 || arrayTwo[i][j] > 9);
+                        } while (arrayTwo[i][j] < 1 || arrayTwo[i][j] > 9);
                     } else {
                         sc.next();
                         System.out.println(MSG_2);
                     }
                 } while (!valorCorrecte);
             }
-            if(i < 6){
-            System.out.println("\n" + MSG_3 + "\n");
+            if (i < 6) {
+                System.out.println("\n" + MSG_3 + "\n");
             }
         }
-        System.out.println(MSG_4);
-        for (i = 0; i < 3 ; i++) {
-            for (j = 0; j < arrayTwo.length; j++) {
-                arrayTwo[j][i] = arrayTwo[i][j];
+        System.out.println("\n" + MSG_4);
+        for (j = 0; j < 3; j++) {
+            for (i = 0; i < arrayTwo.length; i++) {
                 System.out.print(arrayTwo[i][j]);
             }
+            System.out.println();
         }
     }
 }
+//Post: Printem la matriu transposta.
